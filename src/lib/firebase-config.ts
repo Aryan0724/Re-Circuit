8717423c6d177ed4664b2ee352880e3dbe43761c
Aryan Tiwari <aryantiwari0724@gmail.com>
2025-08-25 22:27:00 +0000

    it's taking very long to sign up and after that it says sign up failed,

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAcoz3FPtzMlqmVlhSKhKIIsR9dcv4ESYU",
   authDomain: "re-circuit.firebaseapp.com",
   projectId: "re-circuit",
-  storageBucket: "re-circuit.firebasestorage.app",
+  storageBucket: "re-circuit.appspot.com",
   messagingSenderId: "220008974760",
   appId: "1:220008974760:web:5fa2f57cf573fca058759d",
 };
