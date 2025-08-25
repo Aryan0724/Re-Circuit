62547fcaef707e27e217b38b842645df108c0f2b
Aryan Tiwari <aryantiwari0724@gmail.com>
2025-08-25 19:37:11 +0000

    not working, do you need any api key or something to make this happen

--- next.config.ts
+++ next.config.ts
@@ -23,17 +23,6 @@ const nextConfig: NextConfig = {
       '*.cluster-zkm2jrwbnbd4awuedc2alqxrpk.cloudworkstations.dev',
     ],
   },
-  allowedDevOrigins: [
-    '*.cluster-zkm2jrwbnbd4awuedc2alqxrpk.cloudworkstations.dev',
-  ],
-  env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyAcoz3FPtzMlqmVlhSKhKIIsR9dcv4ESYU",
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "re-circuit.firebaseapp.com",
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: "re-circuit",
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "re-circuit.firebasestorage.app",
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "220008974760",
-    NEXT_PUBLIC_FIREBASE_APP_ID: "1:220008974760:web:5fa2f57cf573fca058759d",
-  },
 };
 
 export default nextConfig;
---
+++ src/lib/firebase-config.ts
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAcoz3FPtzMlqmVlhSKhKIIsR9dcv4ESYU",
+  authDomain: "re-circuit.firebaseapp.com",
+  projectId: "re-circuit",
+  storageBucket: "re-circuit.firebasestorage.app",
+  messagingSenderId: "220008974760",
+  appId: "1:220008974760:web:5fa2f57cf573fca058759d",
+};
